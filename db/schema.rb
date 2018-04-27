696d9f1129ccb8c1f33973585735d2549ad06523
Aubrey Neimeier <neimeier.1@osu.edu>
2018-04-27 16:29:06 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427162734) do
+ActiveRecord::Schema.define(version: 20180427162857) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180427162734) do
     t.datetime "updated_at",       null: false
     t.integer  "category_id"
     t.string   "published_status"
+    t.boolean  "post_status"
   end
 
 end
